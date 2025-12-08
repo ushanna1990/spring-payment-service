e20cc1d16ea8f8994e6fc67c99d3948b8de91d02
ushanna peddapelliwar <peddapelliwarushanna@gmail.com>
2025-12-08 09:25:28 +0530

    spring-9345: renamed file name

--- src/test/java/com/spring/payment/PaymentServiceApplicationTests.java
+++ src/test/java/com/spring/payment/PaymentServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringPaymentServiceApplicationTests {
+class PaymentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
