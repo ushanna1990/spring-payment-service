06d0d0852787f188fc5e75b057f2f6c634e46a0d
ushanna peddapelliwar <peddapelliwarushanna@gmail.com>
2025-12-08 14:27:56 +0530

    Spring-3456: added request mapping

--- src/main/java/com/spring/payment/controller/PaymentController.java
+++ src/main/java/com/spring/payment/controller/PaymentController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/payment")
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
