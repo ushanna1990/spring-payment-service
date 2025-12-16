0785b30f15564e3fb15b17c54a7c165ff6a8ca6c
ushanna peddapelliwar <peddapelliwarushanna@gmail.com>
2025-12-16 18:38:29 +0530

    Spring-3456: Added one endpoint

--- src/main/java/com/spring/payment/service/PaymentService.java
+++ src/main/java/com/spring/payment/service/PaymentService.java
@@ -2,11 +2,13 @@ package com.spring.payment.service;
 
 import com.spring.payment.model.PaymentEntity;
 import com.spring.payment.model.PaymentRequest;
+import com.spring.payment.model.PaymentResponse;
 import com.spring.payment.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PaymentService {
@@ -22,8 +24,24 @@ public class PaymentService {
         return "Payment processed successfully!";
     }
 
-    public Optional<PaymentEntity> viewPaymentStatus(Long id) {
-        return Optional.of(paymentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Payment not found for transaction ID: " + id)));
+    public PaymentResponse viewPayment(Long id) {
+        PaymentEntity paymentEntity = paymentRepository.findById(id).orElse(new PaymentEntity());
+        PaymentResponse paymentResponse = new PaymentResponse();
+        paymentResponse.setAmount(paymentEntity.getAmount());
+        paymentResponse.setPaymentStatus(paymentEntity.getPaymentStatus());
+        paymentResponse.setTransactionId(paymentEntity.getTransactionId());
+        return paymentResponse;
+    }
+
+    public List<PaymentResponse> viewAllPayments() {
+        List<PaymentResponse> paymentResponseList = new ArrayList<>();
+        paymentRepository.findAll().forEach(paymentEntity -> {
+            PaymentResponse paymentResponse = new PaymentResponse();
+            paymentResponse.setAmount(paymentEntity.getAmount());
+            paymentResponse.setPaymentStatus(paymentEntity.getPaymentStatus());
+            paymentResponse.setTransactionId(paymentEntity.getTransactionId());
+            paymentResponseList.add(paymentResponse);
+        });
+        return paymentResponseList;
     }
 }
